288c53ea84aace067679dd35a6693cdd712eb469
vbkoshak <boltrukanisvlad@gmail.com>
2020-09-23 04:07:02 +0300

    fix: add Json.Include.NON_NULL to OrderDTOResponse

--- src/main/java/sb/domain/dto/response/OrderDTOResponse.java
+++ src/main/java/sb/domain/dto/response/OrderDTOResponse.java
@@ -1,5 +1,6 @@
 package sb.domain.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderDTOResponse {
 
     private Integer id;
